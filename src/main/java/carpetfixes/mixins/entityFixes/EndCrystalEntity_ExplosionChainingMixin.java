2751b641286bffcb86b261ff5d70ad4e7dc4acf8
FX <fx.e.morin@gmail.com>
2021-07-06 19:19:24 -0400

    Fixed Rule: `crystalExplodeOnExplodedFix` Fixes #11

--- src/main/java/carpetfixes/mixins/entityFixes/EndCrystalEntity_ExplosionChainingMixin.java
+++ src/main/java/carpetfixes/mixins/entityFixes/EndCrystalEntity_ExplosionChainingMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.decoration.EndCrystalEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EndCrystalEntity.class)
@@ -17,8 +17,9 @@ public class EndCrystalEntity_ExplosionChainingMixin {
      * they would need to implement an explosion list of entity ticking of sorts
      * which is too much for a fix to do.
      */
-    @Inject(method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/damage/DamageSource;isExplosive()Z"),cancellable = true)
-    public void isExplosiveBypass(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(CarpetFixesSettings.crystalExplodeOnExplodedFix || source.isExplosive());
+    @Redirect(method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/damage/DamageSource;isExplosive()Z"))
+    public boolean isExplosiveBypass(DamageSource fakeSource, DamageSource source, float amount) {
+        if (CarpetFixesSettings.crystalExplodeOnExplodedFix) return false;
+        return source.isExplosive();
     }
 }
