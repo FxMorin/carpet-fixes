99ff6d1041db86ae86d8362805ccec63673ae6d9
FX <fx.e.morin@gmail.com>
2021-09-01 16:24:16 -0400

    Screw the compiler warnings xD

--- src/main/java/carpetfixes/mixins/coreSystemFixes/WorldChunk$DirectBlockEntityTickInvoker_illegalBlockEntityMixin.java
+++ src/main/java/carpetfixes/mixins/coreSystemFixes/WorldChunk$DirectBlockEntityTickInvoker_illegalBlockEntityMixin.java
@@ -3,13 +3,12 @@ package carpetfixes.mixins.coreSystemFixes;
 import carpetfixes.CarpetFixesSettings;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.world.chunk.BlockEntityTickInvoker;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Supplier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets="net/minecraft/world/chunk/WorldChunk$DirectBlockEntityTickInvoker")
 public abstract class WorldChunk$DirectBlockEntityTickInvoker_illegalBlockEntityMixin <T extends BlockEntity> implements BlockEntityTickInvoker {
@@ -18,18 +17,17 @@ public abstract class WorldChunk$DirectBlockEntityTickInvoker_illegalBlockEntity
     @Shadow @Final private T blockEntity;
 
 
-    @Redirect(
-            method="tick()V",
+    @Inject(
+            method="tick",
             at=@At(
                     shift= At.Shift.BEFORE,
                     value="INVOKE",
                     target="Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;[Lorg/apache/logging/log4j/util/Supplier;)V"
             ))
-    public void beforeWarnClean(Logger logger, String message, Supplier<?>[] paramSuppliers) {
+    public void beforeWarnClean(CallbackInfo ci) {
         if (CarpetFixesSettings.illegalBlockEntityFix) {
             BlockEntity blockEntity = this.blockEntity;
             blockEntity.getWorld().getChunk(blockEntity.getPos()).removeBlockEntity(blockEntity.getPos());
         }
-        logger.warn(message,paramSuppliers);
     }
 }
