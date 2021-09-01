6eedfff049e291f3b4cf5be67671235d11992ebc
FX <fx.e.morin@gmail.com>
2021-09-01 14:16:59 -0400

    Initialize once

--- src/main/java/carpetfixes/mixins/entityFixes/EndCrystalEntity_InvulnerableMixin.java
+++ src/main/java/carpetfixes/mixins/entityFixes/EndCrystalEntity_InvulnerableMixin.java
@@ -21,6 +21,9 @@ import java.util.Objects;
 public abstract class EndCrystalEntity_InvulnerableMixin extends Entity {
 
 
+    private final BlockPos originalLocation = new BlockPos(0, 128, 0);
+
+
     @Shadow public abstract void setBeamTarget(@Nullable BlockPos beamTarget);
     @Shadow @Nullable public abstract BlockPos getBeamTarget();
 
@@ -33,7 +36,7 @@ public abstract class EndCrystalEntity_InvulnerableMixin extends Entity {
             at=@At("RETURN")
     )
     public void tick(CallbackInfo ci) {
-        if (CarpetFixesSettings.invulnerableEndCrystalFix && this.isInvulnerable() && Objects.equals(this.getBeamTarget(), new BlockPos(0, 128, 0)) && ((this.world.getRegistryKey() == World.END && ((ServerWorld)this.world).getEnderDragonFight() != null && ((((ServerWorld) this.world).getEnderDragonFight().dragonSpawnState != null && ((ServerWorld) this.world).getEnderDragonFight().dragonSpawnState.ordinal() > EnderDragonSpawnState.SUMMONING_DRAGON.ordinal()) || ((ServerWorld) this.world).getEnderDragonFight().dragonSpawnState == null)) || this.world.getRegistryKey() != World.END)) {
+        if (CarpetFixesSettings.invulnerableEndCrystalFix && this.isInvulnerable() && Objects.equals(this.getBeamTarget(), originalLocation) && ((this.world.getRegistryKey() == World.END && ((ServerWorld)this.world).getEnderDragonFight() != null && ((((ServerWorld) this.world).getEnderDragonFight().dragonSpawnState != null && ((ServerWorld) this.world).getEnderDragonFight().dragonSpawnState.ordinal() > EnderDragonSpawnState.SUMMONING_DRAGON.ordinal()) || ((ServerWorld) this.world).getEnderDragonFight().dragonSpawnState == null)) || this.world.getRegistryKey() != World.END)) {
             this.setInvulnerable(false);
             this.setBeamTarget(null);
         }
