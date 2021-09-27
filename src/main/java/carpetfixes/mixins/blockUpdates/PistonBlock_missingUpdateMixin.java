c990f3008810e37d53b2d1c8dab4bdd3924a6d30
FX <fx.e.morin@gmail.com>
2021-09-27 01:04:29 -0400

    Changed `pistonPullingUpdateFix` implementation for game parity

--- src/main/java/carpetfixes/mixins/blockUpdates/PistonBlock_missingUpdateMixin.java
+++ src/main/java/carpetfixes/mixins/blockUpdates/PistonBlock_missingUpdateMixin.java
@@ -2,23 +2,29 @@ package carpetfixes.mixins.blockUpdates;
 
 import carpetfixes.CarpetFixesSettings;
 import net.minecraft.block.PistonBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PistonBlock.class)
 public class PistonBlock_missingUpdateMixin {
 
 
-    @ModifyArg(
+    @Inject(
             method = "move(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;Z)Z",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z",
-                    ordinal = 0
-            ),
-            index = 2
-    )
-    private int pistonUpdate(int value) {return CarpetFixesSettings.pistonPullingUpdateFix ? 21 : 20;}
-
+                    ordinal = 0,
+                    shift= At.Shift.AFTER
+            ))
+    private void move(World world, BlockPos pos, Direction dir, boolean retract, CallbackInfoReturnable<Boolean> cir) {
+        if (CarpetFixesSettings.pistonPullingUpdateFix) {
+            world.updateNeighborsExcept(pos.offset(dir), world.getBlockState(pos).getBlock(), dir.getOpposite());
+        }
+    }
 }
