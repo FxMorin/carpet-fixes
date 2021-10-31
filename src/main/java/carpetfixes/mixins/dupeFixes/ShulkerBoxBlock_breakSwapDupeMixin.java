29c5bee29e23f18bea3554d18aabc247491eb255
FX - PR0CESS <fx.e.morin@gmail.com>
2021-10-31 12:31:30 -0400

    clean

--- src/main/java/carpetfixes/mixins/dupeFixes/ShulkerBoxBlock_breakSwapDupeMixin.java
+++ src/main/java/carpetfixes/mixins/dupeFixes/ShulkerBoxBlock_breakSwapDupeMixin.java
@@ -14,11 +14,13 @@ import java.util.function.Consumer;
 
 @Mixin(ShulkerBoxBlock.class)
 public class ShulkerBoxBlock_breakSwapDupeMixin {
+
     /**
      * Clear the shulker box inventory after its stacks are dropped
      * This might break custom loot tables that intentionally duplicate items
      * but fixes other dupes in vanilla
      */
+
     @Inject(method = "method_10524", at = @At("TAIL"), remap = false)
     private static void clearAfterGetDroppedStack(ShulkerBoxBlockEntity shulkerBox, LootContext lootContext, Consumer<ItemStack> consumer, CallbackInfo ci) {
         if (CarpetFixesSettings.breakSwapGeneralItemDupeFix) {
