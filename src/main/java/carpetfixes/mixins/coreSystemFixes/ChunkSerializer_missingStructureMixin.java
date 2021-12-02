aa433dc36ff4aeed2b8dc2a74c1bc11b0d18ec53
FX - PR0CESS <fx.e.morin@gmail.com>
2021-12-02 10:24:14 -0500

    Forgot this. Fixes crash on generating new world

--- src/main/java/carpetfixes/mixins/coreSystemFixes/ChunkSerializer_missingStructureMixin.java
+++ src/main/java/carpetfixes/mixins/coreSystemFixes/ChunkSerializer_missingStructureMixin.java
@@ -4,10 +4,14 @@ package carpetfixes.mixins.coreSystemFixes;
 import carpetfixes.CarpetFixesSettings;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.ChunkSerializer;
 import net.minecraft.world.chunk.*;
+import net.minecraft.world.chunk.light.LightingProvider;
+import net.minecraft.world.gen.chunk.BlendingData;
 import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraft.world.poi.PointOfInterestStorage;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,6 +20,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.EnumSet;
 import java.util.Map;
 
 @Mixin(ChunkSerializer.class)
@@ -38,17 +43,17 @@ public class ChunkSerializer_missingStructureMixin {
 
     @Inject(
             method = "deserialize",
-            locals = LocalCapture.PRINT,
+            locals = LocalCapture.CAPTURE_FAILSOFT,
             require = 0,
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/chunk/Chunk;setStructureReferences(Ljava/util/Map;)V",
                     shift = At.Shift.AFTER
             ))
-    private static void saveWithoutInvalidStructures(ServerWorld world, PointOfInterestStorage poiStorage, ChunkPos chunkPos, NbtCompound nbt, CallbackInfoReturnable<ProtoChunk> cir) {
-        /*if (ChunkSerializer_missingStructureMixin.preSaveList.get()) {
+    private static void saveWithoutInvalidStructures(ServerWorld world, PointOfInterestStorage poiStorage, ChunkPos chunkPos, NbtCompound nbt, CallbackInfoReturnable<ProtoChunk> cir, UpgradeData upgradeData, boolean bl, NbtList nbtList, int i, ChunkSection chunkSections[], boolean bl2, ChunkManager chunkManager, LightingProvider lightingProvider, Registry registry, long j, ChunkStatus.ChunkType k, BlendingData l, Chunk palettedContainer, NbtCompound palettedContainer2, EnumSet chunkSection, NbtCompound protoChunk) {
+        if (ChunkSerializer_missingStructureMixin.preSaveList.get()) {
             ChunkSerializer_missingStructureMixin.preSaveList.set(false);
-            chunk2.setShouldSave(true);
-        }*/
+            palettedContainer.setShouldSave(true);
+        }
     }
 }
