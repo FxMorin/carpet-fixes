6e02f90105ce59ac954606d69b6558f5d8ca3c2f
FX - PR0CESS <fx.e.morin@gmail.com>
2021-12-15 04:52:35 -0500

    Better code

--- src/main/java/carpetfixes/mixins/dupeFixes/saferItemTransfer/DropperBlock_dontCopyMixin.java
+++ src/main/java/carpetfixes/mixins/dupeFixes/saferItemTransfer/DropperBlock_dontCopyMixin.java
@@ -3,10 +3,14 @@ package carpetfixes.mixins.dupeFixes.saferItemTransfer;
 import carpetfixes.CarpetFixesSettings;
 import net.minecraft.block.DropperBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DropperBlock.class)
 public class DropperBlock_dontCopyMixin {
@@ -22,7 +26,7 @@ public class DropperBlock_dontCopyMixin {
             ))
     protected ItemStack shouldCopyFirst(ItemStack itemStack) {
         stack = itemStack.copy();
-        return CarpetFixesSettings.saferItemTransfers ? itemStack : itemStack.copy();
+        return CarpetFixesSettings.saferItemTransfers ? itemStack : stack;
     }
 
 
@@ -40,5 +44,14 @@ public class DropperBlock_dontCopyMixin {
             ))
     protected ItemStack shouldCopy(ItemStack itemStack) {
         return CarpetFixesSettings.saferItemTransfers ? stack : itemStack.copy();
+    }
+
+
+    @Inject(
+            method = "dispense(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;)V",
+            at = @At("TAIL")
+    )
+    protected void removeInstance(ServerWorld world, BlockPos pos, CallbackInfo ci) {
+        stack = ItemStack.EMPTY;
     }
 }
