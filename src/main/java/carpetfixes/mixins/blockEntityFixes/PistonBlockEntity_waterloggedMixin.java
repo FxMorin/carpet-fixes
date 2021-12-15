c6f6064d410ea5bb7165f230d1ee9ce311a1e4f0
FX - PR0CESS <fx.e.morin@gmail.com>
2021-12-15 07:48:05 -0500

    Actually Added Rule: `pistonsPushWaterloggedBlocksFix`

---
+++ src/main/java/carpetfixes/mixins/blockEntityFixes/PistonBlockEntity_waterloggedMixin.java
@@ -0,0 +1,32 @@
+package carpetfixes.mixins.blockEntityFixes;
+
+import carpetfixes.CarpetFixesSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.PistonBlockEntity;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldAccess;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(PistonBlockEntity.class)
+public class PistonBlockEntity_waterloggedMixin {
+
+
+    @Redirect(
+            method = "finish",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/Block;postProcessState(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"
+            )
+    )
+    public BlockState removeWaterlogged(BlockState state, WorldAccess world, BlockPos pos) {
+        BlockState newState = Block.postProcessState(state, world, pos);
+        if (CarpetFixesSettings.pistonsPushWaterloggedBlocksFix && newState.contains(Properties.WATERLOGGED) && newState.get(Properties.WATERLOGGED)) {
+            newState = newState.with(Properties.WATERLOGGED, false);
+        }
+        return newState;
+    }
+}
