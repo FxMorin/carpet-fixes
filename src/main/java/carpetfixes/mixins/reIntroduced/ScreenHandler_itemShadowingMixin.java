8424e8fd674b0267e2c75ea877a2b32d12e1e3a8
FX - PR0CESS <fx.e.morin@gmail.com>
2021-12-16 10:41:12 -0500

    Fixed issue #25 Rule `reIntroduceItemShadowing` now works as expected

--- src/main/java/carpetfixes/mixins/reIntroduced/ScreenHandler_itemShadowingMixin.java
+++ src/main/java/carpetfixes/mixins/reIntroduced/ScreenHandler_itemShadowingMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,6 +16,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ScreenHandler.class)
 public class ScreenHandler_itemShadowingMixin {
+
+    private int lastButton = 0;
 
 
     @Redirect(
@@ -34,7 +37,7 @@ public class ScreenHandler_itemShadowingMixin {
     )
     private void dontRunBeforeInventoryUpdate(PlayerInventory instance, int slot, ItemStack stack) {
         if (!CarpetFixesSettings.reIntroduceItemShadowing) {
-            instance.setStack(slot, ItemStack.EMPTY);
+            instance.setStack(slot, stack);
         }
     }
 
@@ -79,12 +82,14 @@ public class ScreenHandler_itemShadowingMixin {
     )
     private void dontRunBeforeSecondInventoryUpdate(PlayerInventory instance, int slot, ItemStack stack) {
         if (!CarpetFixesSettings.reIntroduceItemShadowing) {
-            instance.setStack(slot, ItemStack.EMPTY);
+            instance.setStack(slot, stack);
+        } else {
+            lastButton = slot;
         }
     }
 
 
-    @Inject(
+    @Redirect(
             method= "internalOnSlotClick(IILnet/minecraft/screen/slot/SlotActionType;Lnet/minecraft/entity/player/PlayerEntity;)V",
             slice=@Slice(
                     from=@At(
@@ -94,15 +99,15 @@ public class ScreenHandler_itemShadowingMixin {
             ),
             at=@At(
                     value="INVOKE",
-                    target="Lnet/minecraft/screen/slot/Slot;setStack(Lnet/minecraft/item/ItemStack;)V",
-                    ordinal = 4,
-                    shift = At.Shift.AFTER
+                    target="Lnet/minecraft/screen/slot/Slot;onTakeItem(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;)V",
+                    ordinal = 2
             ),
             require = 0
     )
-    private void RunAfterSecondInventoryUpdate(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+    private void RunAfterSecondInventoryUpdate(Slot instance, PlayerEntity player, ItemStack stack) {
         if (CarpetFixesSettings.reIntroduceItemShadowing) {
-            player.getInventory().setStack(button, ItemStack.EMPTY);
+            player.getInventory().setStack(lastButton, stack);
         }
+        instance.onTakeItem(player,stack);
     }
 }
