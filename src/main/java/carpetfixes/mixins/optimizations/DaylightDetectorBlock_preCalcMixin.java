677fcc322eb1047f62b09a956a8e62932b7f8403
FX - PR0CESS <fx.e.morin@gmail.com>
2021-12-24 00:22:56 -0500

    Removed unused classes

--- src/main/java/carpetfixes/mixins/optimizations/DaylightDetectorBlock_preCalcMixin.java
+++
@@ -1,9 +0,0 @@
-package carpetfixes.mixins.optimizations;
-
-import net.minecraft.block.DaylightDetectorBlock;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(DaylightDetectorBlock.class)
-public class DaylightDetectorBlock_preCalcMixin {
-    //24,000
-}
