5e9dfd1f37e44d2ba4e4df0048d6a9fe9781bdd3
FX - PR0CESS <fx.e.morin@gmail.com>
2022-01-30 22:04:39 -0500

    Pretend I did not forget this part

--- src/main/java/carpetfixes/mixins/optimizations/SheepEntity_childColorMixin.java
+++ src/main/java/carpetfixes/mixins/optimizations/SheepEntity_childColorMixin.java
@@ -2,16 +2,22 @@ package carpetfixes.mixins.optimizations;
 
 import carpetfixes.CarpetFixesSettings;
 import carpetfixes.helpers.Utils;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.util.DyeColor;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SheepEntity.class)
-public class SheepEntity_childColorMixin {
+public abstract class SheepEntity_childColorMixin extends AnimalEntity {
+
+    protected SheepEntity_childColorMixin(EntityType<? extends AnimalEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
 
     @Inject(
@@ -19,9 +25,13 @@ public class SheepEntity_childColorMixin {
             at = @At("HEAD"),
             cancellable = true
     )
-    private void getChildColor(AnimalEntity p1, AnimalEntity p2, CallbackInfoReturnable<DyeColor> cir) {
+    private void getChildColor(AnimalEntity firstParent, AnimalEntity secondParent, CallbackInfoReturnable<DyeColor> cir) {
         if (CarpetFixesSettings.optimizedRecipeManager) {
-            cir.setReturnValue(Utils.properDyeMixin(((SheepEntity)p1).getColor(),((SheepEntity)p2).getColor()));
+            DyeColor firstColor = ((SheepEntity)firstParent).getColor();
+            DyeColor secondColor = ((SheepEntity)secondParent).getColor();
+            DyeColor col = Utils.properDyeMixin(firstColor,secondColor);
+            if (col == null) col = this.world.random.nextBoolean() ? firstColor : secondColor;
+            cir.setReturnValue(col);
         }
     }
 }
