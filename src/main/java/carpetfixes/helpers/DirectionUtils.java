f683104a5fcdbde9f444add6fe397d0b09323827
FX - PR0CESS <fx.e.morin@gmail.com>
2022-02-12 16:21:42 -0500

    Added Different direction arrays to DirectionUtils

--- src/main/java/carpetfixes/helpers/DirectionUtils.java
+++ src/main/java/carpetfixes/helpers/DirectionUtils.java
@@ -10,6 +10,14 @@ public class DirectionUtils {
             Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH, Direction.DOWN, Direction.UP
     };
 
+    public static Direction[] extendedUpdateDirections = new Direction[]{
+            Direction.DOWN, Direction.UP, Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST
+    };
+
+    public static Direction[] updateDirections = new Direction[]{
+            Direction.WEST, Direction.EAST, Direction.DOWN, Direction.UP, Direction.NORTH, Direction.SOUTH
+    };
+
     private static final Xoroshiro128PlusPlusRandom random = new Xoroshiro128PlusPlusRandom(0);
 
     public static Direction[] randomDirectionArray(BlockPos pos) {
