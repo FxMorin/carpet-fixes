799d946089c40eba6dfd4f6e4c623ddee24866a7
FX - PR0CESS <fx.e.morin@gmail.com>
2022-03-06 14:42:45 -0500

    Made Carpet isDevelopment Crash Fix much more compatible if carpet wants to use it

--- src/main/java/carpetfixes/mixins/other/Bootstrap_developmentMixin.java
+++ src/main/java/carpetfixes/mixins/other/Bootstrap_developmentMixin.java
@@ -1,22 +1,22 @@
 package carpetfixes.mixins.other;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.Bootstrap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Bootstrap.class)
 public class Bootstrap_developmentMixin {
 
 
-    @Redirect(
+    @ModifyExpressionValue(
             method = "logMissing()V",
             at = @At(
                     value = "FIELD",
                     target = "Lnet/minecraft/SharedConstants;isDevelopment:Z"
             )
     )
-    private static boolean notIsDevelopment() {
+    private static boolean notIsDevelopment(boolean original) {
         return false; //Carpet Crashes if this is ever true!
     }
 }
