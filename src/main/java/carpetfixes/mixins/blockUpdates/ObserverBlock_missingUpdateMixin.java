d4d968eb4bee14c6dcc4e4b59ba146be5a41c074
FX - PR0CESS <fx.e.morin@gmail.com>
2022-03-18 13:55:05 -0400

    Fix `observerUpdateFix` not working

--- src/main/java/carpetfixes/mixins/blockUpdates/ObserverBlock_missingUpdateMixin.java
+++ src/main/java/carpetfixes/mixins/blockUpdates/ObserverBlock_missingUpdateMixin.java
@@ -2,26 +2,32 @@ package carpetfixes.mixins.blockUpdates;
 
 import carpetfixes.CFSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.ObserverBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ObserverBlock.class)
 public class ObserverBlock_missingUpdateMixin {
 
 
-    @ModifyArg(
+    @Inject(
             method = "onBlockAdded(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;" +
                     "Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)V",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/World;" +
-                            "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"
-            ),
-            index = 2
+                            "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z",
+                    shift = At.Shift.AFTER
+            )
     )
-    public int observerUpdate(int val) {
-        return CFSettings.observerUpdateFix ? val | Block.NOTIFY_NEIGHBORS : val;
+    private void observerUpdate(BlockState state, World world, BlockPos pos,
+                                BlockState oldState, boolean notify, CallbackInfo ci) {
+        // They use FORCE_STATE which now requires a separate shape update call
+        if (CFSettings.observerUpdateFix) state.updateNeighbors(world, pos, Block.NOTIFY_LISTENERS);
     }
 }
