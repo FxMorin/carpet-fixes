e540c6aedad75175c01bc5c2e9726b52c9421a84
FX - PR0CESS <fx.e.morin@gmail.com>
2022-04-08 15:32:30 -0400

    Fixed Rule: `tagKeyMemoryLeakFix` crash

--- src/main/java/carpetfixes/mixins/accessors/TagKeyAccessor.java
+++ src/main/java/carpetfixes/mixins/accessors/TagKeyAccessor.java
@@ -3,6 +3,7 @@ package carpetfixes.mixins.accessors;
 import com.google.common.collect.Interner;
 import net.minecraft.tag.TagKey;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(TagKey.class)
@@ -11,7 +12,7 @@ public interface TagKeyAccessor {
     static Interner<TagKey<?>> getInterner() {
         throw new AssertionError();
     }
-    @Accessor("INTERNER")
+    @Accessor("INTERNER") @Mutable
     static void setInterner(Interner<TagKey<?>> interner) {
         throw new AssertionError();
     }
