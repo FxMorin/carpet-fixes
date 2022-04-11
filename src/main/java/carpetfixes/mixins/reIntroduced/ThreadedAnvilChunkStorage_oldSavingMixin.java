57fcd0578644ca4df8e0bcdf3949446777aa93f9
FX - PR0CESS <fx.e.morin@gmail.com>
2022-04-11 03:40:32 -0400

    Possible Fix for `reIntroduceOnlyAutoSaveSaving`

--- src/main/java/carpetfixes/mixins/reIntroduced/ThreadedAnvilChunkStorage_oldSavingMixin.java
+++ src/main/java/carpetfixes/mixins/reIntroduced/ThreadedAnvilChunkStorage_oldSavingMixin.java
@@ -1,25 +1,18 @@
 package carpetfixes.mixins.reIntroduced;
 
 import carpetfixes.CFSettings;
-import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
-import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.ReadOnlyChunk;
-import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.BooleanSupplier;
 
 @Mixin(ThreadedAnvilChunkStorage.class)
 public abstract class ThreadedAnvilChunkStorage_oldSavingMixin {
 
-    @Shadow
+    // Does not seem to be required anymore
+    /*@Shadow
     private volatile Long2ObjectLinkedOpenHashMap<ChunkHolder> chunkHolders;
 
     @Shadow
@@ -42,7 +35,7 @@ public abstract class ThreadedAnvilChunkStorage_oldSavingMixin {
             });
             ci.cancel();
         }
-    }
+    }*/
 
 
     @Redirect(
