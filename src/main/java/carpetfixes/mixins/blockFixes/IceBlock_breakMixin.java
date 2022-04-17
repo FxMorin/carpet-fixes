8ea8eed2b9f38ce1ffc143b9f2be8de383fac690
FX - PR0CESS <fx.e.morin@gmail.com>
2022-04-17 00:33:34 -0400

    New Rule: `iceWaterSkipsWaterloggedFix`

---
+++ src/main/java/carpetfixes/mixins/blockFixes/IceBlock_breakMixin.java
@@ -0,0 +1,32 @@
+package carpetfixes.mixins.blockFixes;
+
+import carpetfixes.CFSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.IceBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(IceBlock.class)
+public class IceBlock_breakMixin {
+
+
+    @Redirect(
+            method = "afterBreak(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;" +
+                    "Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;" +
+                    "Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/item/ItemStack;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)" +
+                            "Lnet/minecraft/block/BlockState;"
+            )
+    )
+    private BlockState getBlockStateModify(World world, BlockPos pos) {
+        BlockState state = world.getBlockState(pos);
+        return CFSettings.iceWaterSkipsWaterloggedFix && state.getFluidState().isStill() ?
+                Blocks.WATER.getDefaultState() : state;
+    }
+}
