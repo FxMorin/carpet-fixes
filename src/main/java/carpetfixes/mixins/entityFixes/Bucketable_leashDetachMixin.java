5db666b80f626704d2f938770c010f2db5111ed9
FX - PR0CESS <fx.e.morin@gmail.com>
2022-04-24 01:35:31 -0400

    New Rule: `bucketableMobsNotDetachingLeashesFix`

---
+++ src/main/java/carpetfixes/mixins/entityFixes/Bucketable_leashDetachMixin.java
@@ -0,0 +1,32 @@
+package carpetfixes.mixins.entityFixes;
+
+import carpetfixes.CFSettings;
+import net.minecraft.entity.Bucketable;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+
+@Mixin(Bucketable.class)
+public interface Bucketable_leashDetachMixin {
+
+
+    @Inject(
+            method = "tryBucket",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/LivingEntity;discard()V"
+            )
+    )
+    private static <T extends LivingEntity & Bucketable> void tryBucket(PlayerEntity player, Hand hand, T entity, CallbackInfoReturnable<Optional<ActionResult>> cir) {
+        if (CFSettings.bucketableMobsNotDetachingLeashesFix && entity instanceof MobEntity mobEntity)
+            mobEntity.detachLeash(false,true);
+    }
+}
