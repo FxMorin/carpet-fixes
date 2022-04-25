2bbfd34db54f8fb6b88ba5bab4474dff62b5828f
FX - PR0CESS <fx.e.morin@gmail.com>
2022-04-25 01:44:01 -0400

    New Rule: `followParentGoalBreaksMovementFix`

---
+++ src/main/java/carpetfixes/mixins/goalFixes/FollowParentGoal_movementMixin.java
@@ -0,0 +1,27 @@
+package carpetfixes.mixins.goalFixes;
+
+import carpetfixes.CFSettings;
+import net.minecraft.entity.ai.goal.FollowParentGoal;
+import net.minecraft.entity.ai.goal.Goal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.EnumSet;
+
+@Mixin(FollowParentGoal.class)
+public abstract class FollowParentGoal_movementMixin extends Goal {
+
+    // Thanks to Chumbanotz for doing the code analysis first
+
+
+    @Inject(
+            method = "canStart()Z",
+            at = @At("RETURN")
+    )
+    private void canStart(CallbackInfoReturnable<Boolean> cir) {
+        if (CFSettings.followParentGoalBreaksMovementFix & cir.getReturnValue())
+            this.setControls(EnumSet.of(Control.MOVE));
+    }
+}
