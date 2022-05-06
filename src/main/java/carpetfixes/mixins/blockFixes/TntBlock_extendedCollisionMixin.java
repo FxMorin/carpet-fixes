8ae906ad18e2707287e77bcaef8b4f82d1ab4641
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-06 02:10:59 -0400

    New Rule: `tntExtendedHitboxClipFix`

---
+++ src/main/java/carpetfixes/mixins/blockFixes/TntBlock_extendedCollisionMixin.java
@@ -0,0 +1,33 @@
+package carpetfixes.mixins.blockFixes;
+
+import carpetfixes.CFSettings;
+import net.minecraft.block.TntBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TntBlock.class)
+public class TntBlock_extendedCollisionMixin {
+
+    // TODO: Add collision check before adding more velocity. Although it's more expensive...
+
+
+    @Redirect(
+            method = "primeTnt(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;" +
+                    "Lnet/minecraft/entity/LivingEntity;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"
+            )
+    )
+    private static boolean adjustVelocityBeforeSpawn(World world, Entity entity) {
+        if (CFSettings.tntExtendedHitboxClipFix) {
+            Vec3d velocity = entity.getVelocity();
+            entity.setVelocity(velocity.x,0.22649273522,velocity.z); // I personally don't like this fix much
+        }
+        return world.spawnEntity(entity);
+    }
+}
