92780923de698628b6cf1b50bff73831523332fc
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-08 15:18:40 -0400

    New Rule: `chorusFruitFallDamageFix`

---
+++ src/main/java/carpetfixes/mixins/itemFixes/ChorusFruitItem_fallDamageMixin.java
@@ -0,0 +1,28 @@
+package carpetfixes.mixins.itemFixes;
+
+import carpetfixes.CFSettings;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ChorusFruitItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ChorusFruitItem.class)
+public class ChorusFruitItem_fallDamageMixin {
+
+
+    @Inject(
+            method = "finishUsing",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/World;playSound(Lnet/minecraft/entity/player/PlayerEntity;" +
+                            "DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V"
+            )
+    )
+    private void finishUsing(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
+        if (CFSettings.chorusFruitFallDamageFix) user.fallDistance = 0;
+    }
+}
