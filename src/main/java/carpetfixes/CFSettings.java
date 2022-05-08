92d8be286997af20bfdd8c857b7b622520ec2806
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-08 16:19:46 -0400

    New Rule: `fallingBlockDamageIsOffsetFix`

--- src/main/java/carpetfixes/CFSettings.java
+++ src/main/java/carpetfixes/CFSettings.java
@@ -1406,6 +1406,14 @@ public class CFSettings {
     )
     public static boolean beaconsAlwaysPlaySoundOnBreakFix = false;
 
+    //By FX - PR0CESS
+    @Rule(
+            desc = "Fixes falling block damage bounding box being offset",
+            extra = "[MC-203302](https://bugs.mojang.com/browse/MC-203302)",
+            category = BUGFIX
+    )
+    public static boolean fallingBlockDamageIsOffsetFix = false;
+
 
     /*
 
