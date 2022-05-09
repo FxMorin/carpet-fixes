a14280f3ee1741f02d798fd71e1ba08306973dc9
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-09 12:25:21 -0400

    Fix #62 & #63 Seems that interface mixins are still buggy, works in the IDE but not in practice! Sadly this makes the fix less compatible for new bucketable mobs currently, so I do want to look into it.

---
+++ src/main/java/carpetfixes/mixins/entityFixes/bucketableLeashDetach/AxolotlEntity_leashDetachMixin.java
@@ -0,0 +1,32 @@
+package carpetfixes.mixins.entityFixes.bucketableLeashDetach;
+
+import carpetfixes.CFSettings;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.AxolotlEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AxolotlEntity.class)
+public abstract class AxolotlEntity_leashDetachMixin extends AnimalEntity {
+
+    protected AxolotlEntity_leashDetachMixin(EntityType<? extends AnimalEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+
+    @Inject(
+            method = "interactMob",
+            at = @At("RETURN")
+    )
+    private void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if (CFSettings.bucketableMobsNotDetachingLeashesFix && this.isLeashed() && cir.getReturnValue().isAccepted())
+            this.detachLeash(false,true);
+    }
+}
---
+++ src/main/java/carpetfixes/mixins/entityFixes/bucketableLeashDetach/FishEntity_leashDetachMixin.java
@@ -0,0 +1,34 @@
+package carpetfixes.mixins.entityFixes.bucketableLeashDetach;
+
+import carpetfixes.CFSettings;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.WaterCreatureEntity;
+import net.minecraft.entity.passive.FishEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FishEntity.class)
+public abstract class FishEntity_leashDetachMixin extends WaterCreatureEntity {
+
+    protected FishEntity_leashDetachMixin(EntityType<? extends WaterCreatureEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    // Added to fish in-case some mod makes it possible to leash fish
+
+
+    @Inject(
+            method = "interactMob",
+            at = @At("RETURN")
+    )
+    private void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if (CFSettings.bucketableMobsNotDetachingLeashesFix && this.isLeashed() && cir.getReturnValue().isAccepted())
+            this.detachLeash(false,true);
+    }
+}
