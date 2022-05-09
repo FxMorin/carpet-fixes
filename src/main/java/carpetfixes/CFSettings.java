4621e33046b3f41b8e269470482224f0c7e07273
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-08 22:43:28 -0400

    New Rule: `obsidianPlatformDestroysBlocksFix`

--- src/main/java/carpetfixes/CFSettings.java
+++ src/main/java/carpetfixes/CFSettings.java
@@ -1705,6 +1705,14 @@ public class CFSettings {
     )
     public static boolean movableCoralFanFix = false;
 
+    //By FX - PR0CESS
+    @Rule(
+            desc = "Fixes the end obsidian platform breaking blocks above the platform every time you travel to the end",
+            extra = "[MC-902](https://bugs.mojang.com/browse/MC-902)",
+            category = BUGFIX
+    )
+    public static boolean obsidianPlatformDestroysBlocksFix = false;
+
 
     /*
 
---
+++ src/main/java/carpetfixes/mixins/coreSystemFixes/ServerWorld_spawnPlatformMixin.java
@@ -0,0 +1,44 @@
+package carpetfixes.mixins.coreSystemFixes;
+
+import carpetfixes.CFSettings;
+import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.minecraft.server.world.ServerWorld.END_SPAWN_POS;
+
+@Mixin(ServerWorld.class)
+public class ServerWorld_spawnPlatformMixin {
+
+
+    @Inject(
+            method = "createEndSpawnPlatform",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void createCustomEndSpawnPlatform(ServerWorld world, CallbackInfo ci) {
+        if (CFSettings.obsidianPlatformDestroysBlocksFix) {
+            BlockPos blockPos = END_SPAWN_POS;
+            int x = blockPos.getX();
+            int y = blockPos.getY() - 2;
+            int z = blockPos.getZ();
+            BlockPos.iterate(x - 2, y + 1, z - 2, x + 2, y + 3, z + 2).forEach(pos -> {
+                if (world.getBlockState(pos).isOf(Blocks.END_STONE))
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState());
+            });
+            BlockPos.iterate(x - 2, y, z - 2, x + 2, y, z + 2).forEach(pos ->
+                    world.setBlockState(pos, Blocks.OBSIDIAN.getDefaultState())
+            );
+            if (!world.isAir(blockPos) || !world.isAir(blockPos.up())) { //obv would do an entityType check instead
+                BlockPos.iterate(x - 1, y + 1, z - 1, x + 1, y + 2, z + 1).forEach(pos -> {
+                    world.breakBlock(pos, true);
+                });
+            }
+            ci.cancel();
+        }
+    }
+}
