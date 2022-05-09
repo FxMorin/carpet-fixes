e4d7d0c5129916745cbaacbe10eeaf8d8d8fb135
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-09 12:35:43 -0400

    Add tadpole to `bucketableMobsNotDetachingLeashesFix` for modded compat

---
+++ src/main/java/carpetfixes/mixins/entityFixes/bucketableLeashDetach/TadpoleEntity_leashDetachMixin.java
@@ -0,0 +1,37 @@
+package carpetfixes.mixins.entityFixes.bucketableLeashDetach;
+
+import carpetfixes.CFSettings;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.FishEntity;
+import net.minecraft.entity.passive.TadpoleEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TadpoleEntity.class)
+public abstract class TadpoleEntity_leashDetachMixin extends FishEntity {
+
+    public TadpoleEntity_leashDetachMixin(EntityType<? extends FishEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    // Added to tadpole in-case some mod makes it possible to leash tadpoles
+
+
+    @Inject(
+            method = "interactMob",
+            at = @At(
+                    value = "RETURN",
+                    ordinal = 1
+            )
+    )
+    private void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if (CFSettings.bucketableMobsNotDetachingLeashesFix && this.isLeashed() && cir.getReturnValue().isAccepted())
+            this.detachLeash(false,true);
+    }
+}
