5442e76287770cbf0c19c88659bd8306a6c878d1
Jack <DevJackMC@gmail.com>
2022-05-13 19:54:02 -0700

    change line spacing, and remove javadoc
     let me know if you want to have JavaDoc for mixins for any future PRs i might send, Please also Tell me if there is a Big issue with the mixin, such as it being a Redirect or its logic, or maybe you don't deem this a bug

--- src/main/java/carpetfixes/mixins/entityFixes/FoxEntity$JumpChasingGoal_TickMixin.java
+++ src/main/java/carpetfixes/mixins/entityFixes/FoxEntity$JumpChasingGoal_TickMixin.java
@@ -13,19 +13,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(targets = "net.minecraft.entity.passive.FoxEntity$JumpChasingGoal")
 public class FoxEntity$JumpChasingGoal_TickMixin {
 
-
-
-    /**
-     * When Foxes Tick, They Check if they Are stuck on snow, But They do not respect the Powder_Snow Block
-     * @return True For Blocks of Snow or Powder_Snow
-     */
-
+    
     @Redirect(method = "tick",at=@At(value = "INVOKE",target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
     public boolean FoxEntity_SnowCheckFix(BlockState instance, Block block) {
 
             return instance.getBlock().equals(Blocks.SNOW) || (CFSettings.foxesDisregardPowderSnowFix && instance.getBlock().equals(Blocks.POWDER_SNOW));
-
-
 
     }
 
