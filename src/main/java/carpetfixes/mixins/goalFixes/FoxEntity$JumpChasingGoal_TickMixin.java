9ba2effea092c57090df5ef531309d32bc7ee42d
Fx Morin <fx.e.morin@gmail.com>
2022-05-15 03:37:35 -0400

    Applied Format

--- src/main/java/carpetfixes/mixins/goalFixes/FoxEntity$JumpChasingGoal_TickMixin.java
+++ src/main/java/carpetfixes/mixins/goalFixes/FoxEntity$JumpChasingGoal_TickMixin.java
@@ -1,6 +1,5 @@
 package carpetfixes.mixins.goalFixes;
 
-
 import carpetfixes.CFSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -9,21 +8,19 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-
 @Mixin(targets = "net.minecraft.entity.passive.FoxEntity$JumpChasingGoal")
 public class FoxEntity$JumpChasingGoal_TickMixin {
 
-
-    @Redirect(method = "tick",at=@At(value = "INVOKE",target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
+    
+    @Redirect(
+        method = "tick",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
+        )
+    )
     public boolean FoxEntity_SnowCheckFix(BlockState instance, Block block) {
-
-            return instance.getBlock().equals(Blocks.SNOW) || (CFSettings.foxesDisreguardPowderSnowFix && instance.getBlock().equals(Blocks.POWDER_SNOW));
-
+            return instance.getBlock().equals(Blocks.SNOW) || (CFSettings.foxesDisreguardPowderSnowFix &&
+                                                               instance.getBlock().equals(Blocks.POWDER_SNOW));
     }
-
-
-
-
-
-
 }
