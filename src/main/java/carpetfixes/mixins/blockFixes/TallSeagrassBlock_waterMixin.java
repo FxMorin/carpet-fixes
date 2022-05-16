25e924794505c8b78c664478a94f85274eb2eb3f
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-16 01:40:01 -0400

    New Rule: `tallGrassWaterWontSpreadFix`

---
+++ src/main/java/carpetfixes/mixins/blockFixes/TallSeagrassBlock_waterMixin.java
@@ -0,0 +1,28 @@
+package carpetfixes.mixins.blockFixes;
+
+import carpetfixes.CFSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TallSeagrassBlock;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.WorldAccess;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TallSeagrassBlock.class)
+public class TallSeagrassBlock_waterMixin extends Block {
+
+    public TallSeagrassBlock_waterMixin(Settings settings) {
+        super(settings);
+    }
+
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
+                                                WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (CFSettings.tallGrassWaterWontSpreadFix)
+            world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+}
