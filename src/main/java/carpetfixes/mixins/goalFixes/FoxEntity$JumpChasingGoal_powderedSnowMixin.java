a729379cbddea6dfef18682094f0b2e96cb30fe2
FX - PR0CESS <fx.e.morin@gmail.com>
2022-05-25 13:16:57 -0400

    Fix FoxEntity issues

--- src/main/java/carpetfixes/mixins/goalFixes/FoxEntity$JumpChasingGoal_powderedSnowMixin.java
+++ src/main/java/carpetfixes/mixins/goalFixes/FoxEntity$JumpChasingGoal_powderedSnowMixin.java
@@ -4,12 +4,13 @@ import carpetfixes.CFSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.passive.FoxEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(targets = "net.minecraft.entity.passive.FoxEntity$JumpChasingGoal")
-public class FoxEntity$JumpChasingGoal_poweredSnowMixin {
+@Mixin(FoxEntity.JumpChasingGoal.class)
+public class FoxEntity$JumpChasingGoal_powderedSnowMixin {
 
     
     @Redirect(
