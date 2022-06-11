b48ef56f85340a4af637cda122a7fee30ec1655f
FX - PR0CESS <fx.e.morin@gmail.com>
2022-06-11 10:09:00 -0400

    Fix crash bug

--- src/main/java/carpetfixes/mixins/entityFixes/ShulkerEntity_vehicleOffsetMixin.java
+++ src/main/java/carpetfixes/mixins/entityFixes/ShulkerEntity_vehicleOffsetMixin.java
@@ -1,26 +1,37 @@
 package carpetfixes.mixins.entityFixes;
 
 import carpetfixes.CFSettings;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.ShulkerEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ShulkerEntity.class)
-public class ShulkerEntity_vehicleOffsetMixin {
+public abstract class ShulkerEntity_vehicleOffsetMixin extends Entity {
+
+    public ShulkerEntity_vehicleOffsetMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
 
-    @SuppressWarnings("InvalidInjectorMethodSignature")
-    @ModifyVariable(
+    @Inject(
             method = "getHeightOffset()D",
+            locals = LocalCapture.CAPTURE_FAILSOFT,
             at = @At(
-                    value = "INVOKE_ASSIGN",
-                    target = "Lnet/minecraft/entity/Entity;getType()Lnet/minecraft/entity/EntityType;"
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/Entity;getType()Lnet/minecraft/entity/EntityType;",
+                    shift = At.Shift.BY,
+                    by = 2
             ),
-            name = "entityType"
+            cancellable = true
     )
-    public EntityType<?> modifyChestBoatToBoat(EntityType<?> type) {
-        return CFSettings.shulkersAreLowerInChestBoatFix && type == EntityType.CHEST_BOAT ? EntityType.BOAT : type;
+    private void modifyShulkerHeightForChestBoat(CallbackInfoReturnable<Double> cir, EntityType<?> type) {
+        if (CFSettings.shulkersAreLowerInChestBoatFix && type == EntityType.CHEST_BOAT)
+            cir.setReturnValue(0.1875 - this.getVehicle().getMountedHeightOffset());
     }
 }
