cd0ae9fe3715d97e1c772bf3cfd64a39c4f19f23
FX - PR0CESS <fx.e.morin@gmail.com>
2022-07-13 19:06:13 -0400

    Fixed #92 Also made the optimization 10% - 22% faster than vanilla!

--- src/main/java/carpetfixes/mixins/coreSystemFixes/DensityFunctionTypes$EndIslands_voidRingsMixin.java
+++ src/main/java/carpetfixes/mixins/coreSystemFixes/DensityFunctionTypes$EndIslands_voidRingsMixin.java
@@ -27,21 +27,23 @@ public class DensityFunctionTypes$EndIslands_voidRingsMixin {
                                            int x, int z, CallbackInfoReturnable<Float> cir) {
         if (CFSettings.endVoidRingsFix) {
             int chunkX = x / 2, chunkZ = z / 2, chunkSectionX = x % 2, chunkSectionZ = z % 2;
-            float noiseShift = (MathHelper.abs(x) < 400 && MathHelper.abs(z) < 400) ?
-                    MathHelper.clamp(400 - MathHelper.sqrt(x * x + z * z) * 8, -100, 80) :
+            float noiseShift = (MathHelper.abs(chunkX) < 200 && MathHelper.abs(chunkZ) < 200) ?
+                    MathHelper.clamp(100.0F - MathHelper.sqrt(x * x + z * z) * 8.0F, -100.0F, 80.0F) :
                     -100;
             for (int islandX = -12; islandX <= 12; ++islandX) {
                 long areaX = (chunkX + islandX);
-                float seedX = (chunkSectionX-islandX*2);
-                for (int islandZ = -12; islandZ <= 12; ++islandZ) {
+                for (int islandZ = -12 + Math.max(islandX-4, 0); islandZ <= 12 + Math.min(islandX+4, 0); ++islandZ) {
                     long areaZ = (chunkZ + islandZ);
                     if (areaX * areaX + areaZ * areaZ > 4096L && simplexNoiseSampler.sample(areaX, areaZ) < -0.9F) {
-                        float seedZ = (chunkSectionZ-islandZ*2);
-                        float shiftPiece = MathHelper.abs(areaX) * 3439.0F + MathHelper.abs(areaZ) * 147.0F;
-                        float offset = shiftPiece % 13.0F + 9.0F;
-                        float shiftOffset = 100.0F - MathHelper.sqrt(seedX*seedX+seedZ*seedZ) * offset;
-                        float maxShift = MathHelper.clamp(shiftOffset, -100.0F, 80.0F);
-                        noiseShift = Math.max(noiseShift, maxShift);
+                        float seedX = chunkSectionX - islandX * 2;
+                        float seedZ = chunkSectionZ - islandZ * 2;
+                        float sqrtSeed = MathHelper.sqrt(seedX * seedX + seedZ * seedZ);
+                        if (sqrtSeed * 9 < (noiseShift * -1) + 100.0005) {
+                            float areaWeight = MathHelper.abs(areaX) * 3439.0F + MathHelper.abs(areaZ) * 147.0F;
+                            float offset = areaWeight % 13.0F + 9.0F;
+                            float maxShift = MathHelper.clamp(100.0F - sqrtSeed * offset, -100.0F, 80.0F);
+                            noiseShift = Math.max(noiseShift, maxShift);
+                        }
                     }
                 }
             }
