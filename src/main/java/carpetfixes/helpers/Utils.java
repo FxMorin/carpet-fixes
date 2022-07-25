9a48958afbd5b35a80d99660c71e3adad7b04a62
FX - PR0CESS <28154542+fmorin@users.noreply.github.com>
2022-07-25 13:58:29 -0400

    Finishing touches for compat

--- src/main/java/carpetfixes/helpers/Utils.java
+++ src/main/java/carpetfixes/helpers/Utils.java
@@ -2,8 +2,6 @@ package carpetfixes.helpers;
 
 import carpetfixes.CarpetFixesServer;
 import carpetfixes.settings.CarpetFixesMixinConfigPlugin;
-import me.fallenbreath.conditionalmixin.api.mixin.ConditionTester;
-import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.Version;
 import net.fabricmc.loader.api.metadata.version.VersionPredicate;
 import net.minecraft.block.Block;
@@ -168,14 +166,4 @@ public class Utils {
             return !((ServerWorld) world).tryLoadEntity(vehicle) ? null : vehicle;
         });
     }
-
-    public static class VMPConditionalPredicate implements ConditionTester {
-
-        @Override
-        public boolean isSatisfied(String mixinClassName) {
-            return FabricLoader.getInstance().isModLoaded("vmp");
-        }
-
-    }
-
 }
--- src/main/java/carpetfixes/mixins/reIntroduced/PlayerManager_LlamaRidingDupeMixin.java
+++ src/main/java/carpetfixes/mixins/reIntroduced/PlayerManager_LlamaRidingDupeMixin.java
@@ -3,14 +3,16 @@ package carpetfixes.mixins.reIntroduced;
 import carpetfixes.CFSettings;
 import carpetfixes.CarpetFixesServer;
 import carpetfixes.helpers.Utils;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -39,9 +41,7 @@ public abstract class PlayerManager_LlamaRidingDupeMixin {
     )
     private @Nullable Entity llamaReplaceOnConnect(NbtCompound nbt, World world,
                                                    Function<Entity, Entity> entityProcessor){
-        if (CFSettings.reIntroduceDonkeyRidingDupe) {
-            Utils.reIntroduceDonkeyRidingDupe_replaceVehicle(nbt, world);
-        }
+        if (CFSettings.reIntroduceDonkeyRidingDupe) Utils.reIntroduceDonkeyRidingDupe_replaceVehicle(nbt, world);
         return EntityType.loadEntityWithPassengers(nbt, world, entityProcessor);
     }
 
@@ -67,4 +67,29 @@ public abstract class PlayerManager_LlamaRidingDupeMixin {
         }
         return false;
     }
+}
+
+
+@Restriction(require = @Condition("vmp"))
+@Mixin(value = PlayerManager.class, priority = 1060)
+abstract class PlayerManager_LlamaRidingDupeMixin_VMPCompat {
+
+
+    @Dynamic
+    @Redirect(
+            method = "vmp$mountSavedVehicles",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/EntityType;loadEntityWithPassengers(" +
+                            "Lnet/minecraft/nbt/NbtCompound;Lnet/minecraft/world/World;" +
+                            "Ljava/util/function/Function;)Lnet/minecraft/entity/Entity;"
+            ),
+            require = 0
+    )
+    private @Nullable Entity llamaReplaceOnConnect(NbtCompound nbt, World world,
+                                                   Function<Entity, Entity> entityProcessor){
+        if (CFSettings.reIntroduceDonkeyRidingDupe) Utils.reIntroduceDonkeyRidingDupe_replaceVehicle(nbt, world);
+        return EntityType.loadEntityWithPassengers(nbt, world, entityProcessor);
+    }
+
 }
