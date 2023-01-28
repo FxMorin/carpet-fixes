c5e66d42509e1721c07746aab3aa6f49d2bd6f9f
Jack <DevJackMC@gmail.com>
2023-01-27 17:19:38 -0800

    fix `MC-120083` with `spectatorAdvancementGrantingFix`

---
+++ src/main/java/carpetfixes/mixins/playerFixes/PlayerAdvancementTracker_grantCriterionMixin.java
@@ -0,0 +1,37 @@
+package carpetfixes.mixins.playerFixes;
+
+import carpetfixes.CFSettings;
+import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.PlayerAdvancementTracker;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameMode;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Fixes an Issue allowing for players in Spectator mode to be granted Advancement Criteria
+ */
+
+
+@Mixin(PlayerAdvancementTracker.class)
+public class PlayerAdvancementTracker_grantCriterionMixin {
+
+    @Shadow
+    private ServerPlayerEntity owner;
+
+    @Inject(
+            method = "grantCriterion",
+            at = @At("HEAD"),
+            cancellable = true)
+    public void grantCriterion(Advancement advancement, String criterionName, CallbackInfoReturnable<Boolean> cir) {
+
+        if (CFSettings.spectatorAdvancementGrantingFix &&
+                owner.interactionManager.getGameMode().equals(GameMode.SPECTATOR))
+            cir.cancel();
+
+
+    }
+}
