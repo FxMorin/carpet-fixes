a56be4c1b2aea949bda6614c18cf17121640bed2
fxmorin <28154542+fxmorin@users.noreply.github.com>
2024-01-18 18:34:14 -0500

    Fix `chestUsablePastWorldBorderFix` - Fixes #160

--- src/main/java/carpetfixes/mixins/blockFixes/ChestBlock_worldBorderMixin.java
+++ src/main/java/carpetfixes/mixins/blockFixes/ChestBlock_worldBorderMixin.java
@@ -1,17 +1,11 @@
 package carpetfixes.mixins.blockFixes;
 
 import carpetfixes.CFSettings;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.ChestBlock;
-import net.minecraft.block.enums.ChestType;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.state.property.DirectionProperty;
-import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,16 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(ChestBlock.class)
 public class ChestBlock_worldBorderMixin {
-
-    @Shadow
-    @Final
-    public static DirectionProperty FACING;
-
-    @Shadow
-    @Final
-    public static EnumProperty<ChestType> CHEST_TYPE;
-
-    ChestBlock self = (ChestBlock)(Object)this;
 
 
     @Inject(
@@ -44,11 +28,8 @@ public class ChestBlock_worldBorderMixin {
         if (CFSettings.chestUsablePastWorldBorderFix) {
             BlockPos blockPos = ctx.getBlockPos().offset(dir);
             if (!ctx.getWorld().getWorldBorder().contains(blockPos)) {
-                BlockState blockState = ctx.getWorld().getBlockState(blockPos);
-                if (blockState.isOf(self) && blockState.get(CHEST_TYPE) == ChestType.SINGLE)
-                    cir.setReturnValue(blockState.get(FACING));
+                cir.setReturnValue(null);
             }
-            cir.setReturnValue(null);
         }
     }
 }
